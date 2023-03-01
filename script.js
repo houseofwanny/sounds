60362916a5e8c1e57e75ba62a33de1fb8324f63e
houseofwanny <112391506+houseofwanny@users.noreply.github.com>
2023-03-01 12:12:26 +0100

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("Hello Guys");
+
+
+function removeTansition(e){
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTansition));
+window.addEventListener('keydown',playSound);
